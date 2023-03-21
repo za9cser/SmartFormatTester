2d5d386b0d0b8fe0b5605864a251ed70fa06c58b
Sergei Zaitsev <zaitsev.sergey.93@gmail.com>
2023-03-21 22:36:57 +0300

    feat: boilerplate with docker created

---
+++ SmartFormatTester/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    "default",
+    "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
