abb329b0f11a2f0f273d812dd4a71dae1438b1bf
Sergei Zaitsev <zaitsev.sergey.93@gmail.com>
2023-03-22 07:44:25 +0300

    feat: design updated with smartformat to place near both expression and it result

--- SmartFormatTester/ClientApp/src/modules/smartFormat/index.jsx
+++ SmartFormatTester/ClientApp/src/modules/smartFormat/index.jsx
@@ -4,10 +4,15 @@ import * as yup from "yup";
 import { MDBContainer, MDBRow, MDBCol, MDBInputGroup, MDBInput, MDBBtn } from "mdb-react-ui-kit";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy } from "@fortawesome/free-regular-svg-icons";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faPlay, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const initialValues = {
-    smartFormatExpressions: [""],
+    smartFormats: [
+        {
+            expression: "",
+            result: "",
+        },
+    ],
     model: "",
 };
 
@@ -17,49 +22,90 @@ const SmartFormat = () => {
             {(formik) => (
                 <Form>
                     <MDBContainer>
-                        <div className="border-bottom">
-                            <FieldArray name="smartFormatExpressions">
-                                {({ push, remove }) => (
-                                    <MDBContainer>
-                                        {formik.values.smartFormatExpressions.map((expression, index) => (
+                        <FieldArray name="smartFormats">
+                            {({ push, remove }) => (
+                                <MDBContainer>
+                                    {formik.values.smartFormats.map((_, index) => (
+                                        <MDBContainer className="border-top border-bottom py-1">
+                                            <MDBRow start className="fw-bold">{`SmartFormat expression ${
+                                                index + 1
+                                            }`}</MDBRow>
                                             <MDBRow className="align-items-center mb-3">
                                                 <MDBCol
                                                     md={"auto"}
                                                     className="px-0"
-                                                    title={`Copy Smart Format expression ${index + 1}`}
+                                                    title={`Copy SmartFormat ${index + 1} expression`}
                                                 >
-                                                    <MDBBtn color="light" className="px-2">
+                                                    <MDBBtn color="light" className="px-3" size="lg">
                                                         <FontAwesomeIcon icon={faCopy} color="text-primary" size="lg" />
                                                     </MDBBtn>
                                                 </MDBCol>
                                                 <MDBCol>
                                                     <MDBInput
-                                                        label={`Smart Format expression ${index + 1}`}
-                                                        name={`smartFormatExpressions[${index}]`}
+                                                        label={`Smart Format ${index + 1} expression`}
+                                                        name={`smartFormats[${index}].expression`}
                                                         type="text"
                                                         className="w-100"
+                                                        size="lg"
                                                     />
                                                 </MDBCol>
+                                                <MDBCol md={"auto"}>
+                                                    <MDBBtn
+                                                        color="primary"
+                                                        onClick={() => remove(index)}
+                                                        className="px-3"
+                                                        size="lg"
+                                                    >
+                                                        <FontAwesomeIcon icon={faPlay} size="lg" />
+                                                    </MDBBtn>
+                                                </MDBCol>
+
                                                 <MDBCol md={"auto"}>
                                                     <MDBBtn
                                                         color="danger"
                                                         onClick={() => remove(index)}
                                                         className="px-3"
+                                                        size="lg"
                                                     >
-                                                        <FontAwesomeIcon icon={faTimes} />
+                                                        <FontAwesomeIcon icon={faTimes} size="lg" />
                                                     </MDBBtn>
                                                 </MDBCol>
                                             </MDBRow>
-                                        ))}
-                                        <MDBRow start>
-                                            <MDBBtn color="link" onClick={() => push("")}>
-                                                Add Smart Format Expression
-                                            </MDBBtn>
-                                        </MDBRow>
-                                    </MDBContainer>
-                                )}
-                            </FieldArray>
-                        </div>
+                                            <MDBRow>
+                                                <MDBRow start className="fw-bold">
+                                                    {`Smart Format ${index + 1} result`}
+                                                </MDBRow>
+                                            </MDBRow>
+                                            <MDBRow className="align-items-center mb-3">
+                                                <MDBCol
+                                                    md={"auto"}
+                                                    className="px-0"
+                                                    title={`Copy Smart Format ${index + 1} result`}
+                                                >
+                                                    <MDBBtn color="light" className="px-3" size="lg">
+                                                        <FontAwesomeIcon icon={faCopy} color="text-primary" size="lg" />
+                                                    </MDBBtn>
+                                                </MDBCol>
+                                                <MDBCol>
+                                                    <MDBInput
+                                                        label={`Smart Format ${index + 1} result`}
+                                                        name={`smartFormats[${index}].result`}
+                                                        type="text"
+                                                        className="w-100"
+                                                        size="lg"
+                                                    />
+                                                </MDBCol>
+                                            </MDBRow>
+                                        </MDBContainer>
+                                    ))}
+                                    <MDBRow start>
+                                        <MDBBtn color="link" onClick={() => push("")}>
+                                            Add Smart Format Expression
+                                        </MDBBtn>
+                                    </MDBRow>
+                                </MDBContainer>
+                            )}
+                        </FieldArray>
                     </MDBContainer>
                 </Form>
             )}
