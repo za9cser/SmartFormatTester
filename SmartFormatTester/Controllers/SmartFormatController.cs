af9b8d2cb93ceb01c19fbb527ed61cb9d14e4e3c
Sergei Zaitsev <zaitsev.sergey.93@gmail.com>
2023-03-22 19:24:55 +0300

    feat: smart format added

--- SmartFormatTester/Controllers/SmartFormatController.cs
+++ SmartFormatTester/Controllers/SmartFormatController.cs
@@ -11,4 +11,4 @@ public class SmartFormatController : Controller
     {
         return Ok();
     }
-}   
+}
--- SmartFormatTester/Program.cs
+++ SmartFormatTester/Program.cs
@@ -1,8 +1,19 @@
+using System.Text.RegularExpressions;
+using SmartFormat;
+using SmartFormat.Core.Settings;
+using SmartFormat.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+
+Smart.Default.Settings.FormatErrorAction = ErrorAction.Ignore;
+Smart.Default.Settings.ParseErrorAction = ErrorAction.Ignore;
+// вырубаем форматтеры даты м времени от SmartFormat, чтобы работали стандартные .net форматтеры
+Smart.Default.FormatterExtensions.RemoveAll(f => f is TimeFormatter);
+Smart.Default.FormatterExtensions.Add(new IsMatchFormatter { RegexOptions = RegexOptions.CultureInvariant | RegexOptions.IgnoreCase });
 
 var app = builder.Build();
 
